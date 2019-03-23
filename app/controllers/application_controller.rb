ad0f2ac8017b2230301e3d56419bca2ef51d0f2e
itoitto <arasi_rainbow@yahoo.co.jp>
2019-03-23 13:29:29 +0000

    ログイン、ログアウト後のリダイレクト定義修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  # ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource_or_scope)
+    posts_index_path(resource_or_scope)
+  end
+
+  # ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin_admin_user
+      new_admin_admin_user_session_path
+    else
+      new_user_session_path
+    end
+  end
+
 end

6438ab9479c7b92dc2072b53926026b5ac94256d
itoitto <arasi_rainbow@yahoo.co.jp>
2019-03-26 21:15:45 +0000

    フッター実装

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -4,6 +4,21 @@ class ProfileController < ApplicationController
   end
 
   def update
+    @user = User.find_by(id: current_user.id)
+    @user.id = current_user.id
+    @user.email = params[:email]
+    @user.username = params[:username]
+    @user.password = params[:password]
+    if params[:user_pic]
+      image = params[:user_pic]
+      @user.image_name = "#{current_user.id}.jpg"
+      File.binwrite("public/image/#{@user.image_name}",image.read)
+    end
+    if @user.save
+      flash[:notice]="ユーザー情報の更新に成功しました。"
+    else
+      flash[:notice]="ユーザー情報の更新に失敗しました。"
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/authentication'
   get "profile/edit" => "profile#edit"
+  post "profile/:id/update" => "profile#update"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # devise_for :users, :controllers => {
   # :registrations => 'users/registrations',

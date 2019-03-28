6575e7577ab3c1cf9f3f72dcbbed0030db198629
itoitto <arasi_rainbow@yahoo.co.jp>
2019-03-28 12:06:09 +0000

    flash[:notice]追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   validates :practice_name, {presence: true}
-  validates :practice_content, {presence: true}
+  validates :practice_content, {presence: true, length: {maximum: 140}}
   validates :practice_date, {presence: true}
 end

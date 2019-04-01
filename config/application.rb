d7d3a2183a3324e1f617b86d5a593b30747009ca
itoitto <arasi_rainbow@yahoo.co.jp>
2019-04-01 16:18:20 +0100

    レイアウト崩れ対応

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
 module SalogApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -13,6 +12,7 @@ module SalogApp
     config.i18n.default_locale = :ja
     config.active_record.default_timezone = :local
     config.time_zone = 'Tokyo'
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
